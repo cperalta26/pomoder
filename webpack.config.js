2dd2e65ec2902661c13c92f0cca47f3347733889
Carmen Peralta <cperalta26@gmail.com>
2019-02-04 18:13:54 -0500

    add(webpack and .gitignore): setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.jsx', '.js', '.json', '*']
+  }, 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+              sourceMap: true,
+              localIdentName: '[local]---[hash:base64:5]'
+            }
+          }
+        ]
+      }, 
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]?[hash]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
