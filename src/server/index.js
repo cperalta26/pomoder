cafae15d74ff77a104d86bc814a0f72e4755727c
Carmen Peralta <cperalta26@gmail.com>
2019-02-04 18:49:16 -0500

    add(index.js): express server

---
+++ src/server/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res, next) => res.send('<h1>Connected to boiler maker on port 3000</h1>'))
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
