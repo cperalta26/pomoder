119de5a7c392130f5f6a513892a0d3646689cbd4
Carmen Peralta <cperalta26@gmail.com>
2019-02-11 15:26:58 -0500

    fix(webpack, index): add semicolons

--- src/server/index.js
+++ src/server/index.js
@@ -1,34 +1,34 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const path = require('path')
-const chalk = require('chalk')
-app.use(express.static(path.join(__dirname, '../..', 'public' )))
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require('path');
+const chalk = require('chalk');
+app.use(express.static(path.join(__dirname, '../..', 'public' )));
 
 // logging middleware 
-const morgan = require('morgan')
-app.use(morgan('dev'))
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 // parsing middleware
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // 404 error handling
 app.get('*', (req, res, next) => {
-  const err = new Error('Page Not Found')
-  err.statusCode = 404
-  next(err)
-})
+  const err = new Error('Page Not Found');
+  err.statusCode = 404;
+  next(err);
+});
 
 // internal error handling 
 app.use((err, req, res, next) => {
   console.error(err);
   console.error(err.stack);
-  res.status(err.status || 500).send(err.message || 'Internal server error.')
-})
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
 
 // starting the server
 app.listen(port, () => {
-  console.log(chalk.cyan.bold(`Listening on port ${port}`))
-})
+  console.log(chalk.cyan.bold(`Listening on port ${port}`));
+});
