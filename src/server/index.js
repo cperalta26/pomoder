396c5f1a0877be4d03790169facf7d81f10e4c23
Carmen Peralta <cperalta26@gmail.com>
2019-02-11 12:25:14 -0500

    setup(server): add logging middleware

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 3000
 const path = require('path')
 app.use(express.static(path.join(__dirname, '../..', 'public' )))
 
+// logger middleware setup
+const morgan = require('morgan')
+app.use(morgan('dev'))
+
 // starting the server
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
