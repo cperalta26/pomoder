3fe60d2ebd7b643f9ea8ac29c69e98e112aea09a
Carmen Peralta <cperalta26@gmail.com>
2019-02-12 17:32:16 -0500

    create NotFound component

--- src/client/MainApp.js
+++ src/client/MainApp.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 // houses our routes and all of our components
 export default class App extends Component {
@@ -14,6 +15,7 @@ export default class App extends Component {
     return (
       <Switch>
         <Route exact path ="/" component={Home} />
+        <Route component={NotFound} />
       </Switch>
     );
   }
