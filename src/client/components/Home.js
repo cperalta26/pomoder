f2406dcf931559a2686ab0c9de194149ff93166d
Carmen Peralta <cperalta26@gmail.com>
2019-02-18 12:25:56 -0500

    home.js - delete console.log

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Home = (props) => {
-  console.log(`these are the props coming in: ${JSON.stringify(props)}`)
   return (
     <h1>This is a pomodoro app!!!!</h1>
   )
