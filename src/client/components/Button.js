2e2546fe2bae2f28ff90c6190634e9331f7c825a
Carmen Peralta <cperalta26@gmail.com>
2019-02-19 21:43:27 -0500

    fix(Button): duplicate color

--- src/client/components/Button.js
+++ src/client/components/Button.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  color: #fff;
   font-family: 'Karla', sans-serif;
   font-weight: 700;
   font-align: center;
