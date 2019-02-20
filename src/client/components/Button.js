a7b7eb28f2dedeb0dc492900e38325c4ca2ec0e2
Carmen Peralta <cperalta26@gmail.com>
2019-02-19 21:36:11 -0500

    feat(Button): style button

--- src/client/components/Button.js
+++ src/client/components/Button.js
@@ -7,6 +7,14 @@ const StyledButton = styled.button`
   font-weight: 700;
   font-align: center;
   font-size: 13px;
+
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #645DE9;
+  border: 3px solid #645DE9;
+  paddin: 3px;
+  color: #fff;
 `;
 
 const Button = () => (
