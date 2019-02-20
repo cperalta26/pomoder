6b1fed98f8e196eb5e8be14b9ee1b9c59570b878
Carmen Peralta <cperalta26@gmail.com>
2019-02-19 20:34:44 -0500

    fix(Button): delete px on font-weight

--- src/client/components/Button.js
+++ src/client/components/Button.js
@@ -1,10 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
+const StyledButton = styled.button`
+  color: #fff;
+  font-family: 'Karla', sans-serif;
+  font-weight: 700;
+  font-align: center;
+`;
+
 const Button = () => (
-  <div>
-    this will be a button
-  </div>
+  <StyledButton>
+    <h1>Pomoder</h1>
+  </StyledButton>
 );
 
 export default Button;
