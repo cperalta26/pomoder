c7033376de86c1d86d3a14decd69551c4e0250be
Carmen Peralta <cperalta26@gmail.com>
2019-02-22 18:21:48 -0500

    feat(PomoderContainer): arrange timer and buttons

--- src/client/components/PomoderContainer.js
+++ src/client/components/PomoderContainer.js
@@ -8,19 +8,35 @@ const Container = styled.div`
   margin: 0 5vw;
   height: 100vh;
   display: grid;
-  grid-template-rows: 10% repeat(1fr);
+  grid-template-rows: 1fr 4fr;
+`;
+
+const TimerAndButtonsContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  grid-template-columns: 1fr 2fr 1fr;
+`;
+
+const TimerGrid = styled.div`
+  display: grid;
+  grid-row: 1/-1;
+  grid-column: 2;
 `;
 
 const PomoderContainer = (props) =>  (
   <Container> 
     <h1>Pomoder</h1>
-    <Button btnColor='#645DE9' name='Pomodoro'/>
-    <Timer />
-    <Button btnColor='#B73FF3' name='Long Break'/>
-    <Button btnColor='#F75789' name='Short Break'/>
-    <Button btnColor='#2BBABD' name='Start'/>
-    <Button btnColor='#F5243E' name='Stop'/>
-    <Button btnColor='#3AB9EC' name='Reset'/>
+    <TimerAndButtonsContainer>
+      <Button btnColor='#645DE9' name='Pomodoro'/>
+      <Button btnColor='#B73FF3' name='Long Break'/>
+      <Button btnColor='#F75789' name='Short Break'/>
+      <Button btnColor='#2BBABD' name='Start'/>
+      <Button btnColor='#F5243E' name='Stop'/>
+      <Button btnColor='#3AB9EC' name='Reset'/>
+      <TimerGrid>
+        <Timer/>
+      </TimerGrid>
+    </TimerAndButtonsContainer>
   </Container>
 );
 
