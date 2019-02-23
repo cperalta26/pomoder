47c34f770571b8e4a786f304fc3a4e3f5b05558a
Carmen Peralta <cperalta26@gmail.com>
2019-02-23 17:45:00 -0500

    feat(Button): make button bigger

--- src/client/components/Button.js
+++ src/client/components/Button.js
@@ -5,10 +5,10 @@ const StyledButton = styled.button`
   font-family: 'Karla', sans-serif;
   font-weight: 700;
   font-align: center;
-  font-size: 13px;
+  font-size: 18px;
 
-  width: 100px;
-  height: 100px;
+  width: 115px;
+  height: 115px;
   border-radius: 50%;
   background-color: ${props => props.color};
   border: 3px solid ${props => props.color};
