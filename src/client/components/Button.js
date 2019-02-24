7db3eec823641400899f32e633d9dc2d07c213f7
Carmen Peralta <cperalta26@gmail.com>
2019-02-23 22:56:34 -0500

    fix(Button): text alignment

--- src/client/components/Button.js
+++ src/client/components/Button.js
@@ -2,22 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  font-family: 'Karla', sans-serif;
-  font-weight: 700;
-  font-align: center;
-  font-size: 18px;
-
   width: 95%;
 	height: 0%;
 	padding-bottom: 90%;
   border-radius: 50%;
   background-color: ${props => props.color};
   border: 3px solid ${props => props.color};
+  overflow: hidden;
+`;
+
+const StyledText = styled.div`
   color: #fff;
+  line-height: 1em;
+  font-family: 'Karla', sans-serif;
+  font-weight: 700;
+  font-size: 18px;
+  padding: 40% 0%; 
 `;
 
 const Button = ({btnColor, name}) => (
-  <StyledButton color={btnColor}>{name}</StyledButton>
+  <StyledButton color={btnColor}>
+    <StyledText>
+      {name}
+    </StyledText>
+  </StyledButton>
 );
 
 export default Button;
