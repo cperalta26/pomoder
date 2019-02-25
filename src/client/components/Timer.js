0badee67bc45db199e617b40f1166da0c6e60133
Carmen Peralta <cperalta26@gmail.com>
2019-02-25 15:37:27 -0500

    feat(Timer): add inner circle

--- src/client/components/Timer.js
+++ src/client/components/Timer.js
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledTimer = styled.svg`
+  & .fade-stop-1 {
+    stop-color: #44479b; 
+    stop-opacity: 100;
+  }
+
+  & .fade-stop-2 {
+    stop-color: #1c1d3d;
+    stop-opacity: 1;
+  }
+`;
 
 const Timer = () => (
-  <h1>TIMER!!!!</h1>
+  <StyledTimer viewBox="0 0 50 50">
+    <defs>
+      <radialGradient id="fade" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+        <stop offset="0%" className="fade-stop-1" />
+        <stop offset="100%" className="fade-stop-2"/>
+      </radialGradient>
+    </defs>
+    <circle
+      cx="25"
+      cy="25"
+      r="20"
+      fill= "url(#fade)"
+      strokeWidth="4"
+    />
+  </StyledTimer>
 );
 
 export default Timer;
