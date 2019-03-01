3e259e436d1e6b20a1b92f9e0a506958656315bd
Carmen Peralta <cperalta26@gmail.com>
2019-02-28 21:25:41 -0500

    chore(Timer): change to stateful component

--- src/client/components/Timer.js
+++ src/client/components/Timer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import styled from "styled-components";
 
 const StyledTimer = styled.svg`
@@ -28,39 +28,43 @@ const StyledTimer = styled.svg`
   }
 `;
 
-const Timer = ({countdownTime, countdownTimeColor}) => { 
-  // console.log('countdownTimer is ' + JSON.stringify(countdownTime))
-   
-  return (
-    <StyledTimer viewBox="0 0 50 50" countdownTimeColor={countdownTimeColor}>
-      <defs>
-        <radialGradient id="fade" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-          <stop offset="0%" className="fade-stop-1" />
-          <stop offset="100%" className="fade-stop-2"/>
-        </radialGradient>
-      </defs>
-      <circle
-        cx="25"
-        cy="25"
-        r="20"
-        fill="url(#fade)"
-        strokeWidth="4"
-      />
-      <path className="circle"
-        d="M25 5
-            a 20 20 0 0 1 0 40
-            a 20 20 0 0 1 0 -40"
-      />
-      <text className="text"
-        x="25"
-        y="25"
-        dy=".3em"
-        textAnchor="middle"
-      >
-        {`${countdownTime.minutes}: ${countdownTime.seconds}`}
-      </text>
-    </StyledTimer>
-  )
-};
+export default class Timer extends Component { 
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-export default Timer;
+  render() {  
+    console.log('now a stateful component')
+    return (
+      <StyledTimer viewBox="0 0 50 50" countdownTimeColor={this.props.countdownTimeColor}>
+        <defs>
+          <radialGradient id="fade" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+            <stop offset="0%" className="fade-stop-1" />
+            <stop offset="100%" className="fade-stop-2"/>
+          </radialGradient>
+        </defs>
+        <circle
+          cx="25"
+          cy="25"
+          r="20"
+          fill="url(#fade)"
+          strokeWidth="4"
+        />
+        <path className="circle"
+          d="M25 5
+              a 20 20 0 0 1 0 40
+              a 20 20 0 0 1 0 -40"
+        />
+        <text className="text"
+          x="25"
+          y="25"
+          dy=".3em"
+          textAnchor="middle"
+        >
+          {`${this.props.countdownTime.minutes}: ${this.props.countdownTime.seconds}`}
+        </text>
+      </StyledTimer>
+    )
+  }
+};
