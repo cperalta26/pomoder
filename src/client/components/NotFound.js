529fa7858c9ab3a6ecfd4018456bb1657d640a66
Carmen Peralta <cperalta26@gmail.com>
2019-03-14 19:06:17 -0400

    style the NotFound component

--- src/client/components/NotFound.js
+++ src/client/components/NotFound.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import styled from 'styled-components';
 
-const NotFound = () => (<h1>This page was not found</h1>);
+const StyledNotFound = styled.div`
+  color: #fff;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 700;
+  line-height: 45px;
+
+  width: 100vw;
+  height: 100vh;
+
+  display: grid;
+  justify-content: center;
+  align-content: center;
+`;
+
+const NotFound = () => (
+  <StyledNotFound>
+    <h1>This page was not found</h1>
+    <Link to="/">Go back to home page</Link>
+  </StyledNotFound>
+);
 
 export default NotFound;
