a23a8fc6e34616df001c6e12d9744209640ac8bb
Carmen Peralta <cperalta26@gmail.com>
2019-04-16 16:29:13 -0400

    fix open handles when testing

--- src/server/index.test.js
+++ src/server/index.test.js
@@ -2,15 +2,19 @@ const request = require('supertest');
 const server = require('./index');
 
 describe('GET status', () => {
-  it('/ responds with 200', () => {
-    request(server).get('/', (_, res) => {
-      expect(res.statusCode).to.equal(200);
-    });
+  it('/ responds with 200', (done) => {
+    request(server)
+      .get('/', (_, res) => {
+        expect(res.statusCode).to.equal(200);
+      })
+      .end(done);
   });
 
-  it('/* also responds with 200', () => {
-    request(server).get('/asdf', (_, res) => {
-      expect(res.statusCode).to.equal(200);
-    });
+  it('/* also responds with 200', (done) => {
+    request(server)
+      .get('/asdf', (_, res) => {
+        expect(res.statusCode).to.equal(200);
+      })
+      .end(done);
   });
 });
